9e38f32c33fbed62c98a1cb620b5a8bfc48809ab
Rizky nugroho <nugrizky72@gmail.com>
2024-10-28 09:18:14 +0700

    program fix enemy
    pake yg control enemy biasa

---
+++ Projek game 3D Fix banget/Assets/Script/SceneLoaer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaer : MonoBehaviour
+{
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
