7f73175dd19005dde9b5f406de72197c02c9ec8b
Rizky nugroho <nugriiz07@gmail.com>
2024-11-16 15:35:19 +0700

    Update BackgroundMusic.cs

--- Projek game 3D Fix banget/Assets/BackgroundMusic.cs
+++ Projek game 3D Fix banget/Assets/BackgroundMusic.cs
@@ -1,18 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackgroundMusic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static BackgroundMusic instance;
+    private AudioSource audioSource;
+
+    void Awake()
     {
-        
+        // Jika sudah ada instance lain, hapus objek ini
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Agar musik tetap ada ketika pindah scene
+        }
+
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        // Memastikan AudioSource ada dan teratur
+        if (audioSource != null && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    void OnEnable()
+    {
+        // Menambahkan listener untuk event ketika scene baru dimuat
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        // Menghapus listener saat objek dihancurkan
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    // Fungsi ini dipanggil setiap kali scene baru dimuat
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Jika sudah masuk ke scene selain main menu, hentikan musik
+        if (scene.name != "MainMenu") // Ganti "MainMenu" dengan nama scene menu Anda
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+            Destroy(gameObject); // Hapus objek musik setelah berhenti
+        }
     }
 }
